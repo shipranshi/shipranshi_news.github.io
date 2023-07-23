466d52149d465fe70a793ff667c485e7100f39ff
shipranshi keshri <100595095+shipranshi@users.noreply.github.com>
2023-07-24 01:54:20 +0530

    news

---
+++ script.js
@@ -0,0 +1,147 @@
+const API_KEY = "c44737fbff70f9e64643f76f9b0c66fc";
+const url = "https://gnews.io/api/v4/search?q=";
+
+// window.addEventListener("load", () => fetchNews("India"));
+
+// function reload() {
+//     window.location.reload();
+// }
+
+// async function fetchNews(query) {
+//     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+//     const data = await res.json();
+//     bindData(data.articles);
+// }
+
+// function bindData(articles) {
+//     const cardsContainer = document.getElementById("cards-container");
+//     const newsCardTemplate = document.getElementById("template-news-card");
+
+//     cardsContainer.innerHTML = "";
+
+//     articles.forEach((article) => {
+//         if (!article.urlToImage) return;
+//         const cardClone = newsCardTemplate.content.cloneNode(true);
+//         fillDataInCard(cardClone, article);
+//         cardsContainer.appendChild(cardClone);
+//     });
+// }
+
+// function fillDataInCard(cardClone, article) {
+//     const newsImg = cardClone.querySelector("#news-img");
+//     const newsTitle = cardClone.querySelector("#news-title");
+//     const newsSource = cardClone.querySelector("#news-source");
+//     const newsDesc = cardClone.querySelector("#news-desc");
+
+//     newsImg.src = article.urlToImage;
+//     newsTitle.innerHTML = article.title;
+//     newsDesc.innerHTML = article.description;
+
+//     const date = new Date(article.publishedAt).toLocaleString("en-US", {
+//         timeZone: "Asia/Jakarta",
+//     });
+
+//     newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+//     cardClone.firstElementChild.addEventListener("click", () => {
+//         window.open(article.url, "_blank");
+//     });
+// }
+
+// let curSelectedNav = null;
+// function onNavItemClick(id) {
+//     fetchNews(id);
+//     const navItem = document.getElementById(id);
+//     curSelectedNav?.classList.remove("active");
+//     curSelectedNav = navItem;
+//     curSelectedNav.classList.add("active");
+// }
+
+// const searchButton = document.getElementById("search-button");
+// const searchText = document.getElementById("search-text");
+
+// searchButton.addEventListener("click", () => {
+//     const query = searchText.value;
+//     if (!query) return;
+//     fetchNews(query);
+//     curSelectedNav?.classList.remove("active");
+//     curSelectedNav = null;
+// });
+
+
+
+// const API_KEY = "a50e903b6a829039d24a0ebd03dc1b2d";
+// const url = "https://gnews.io/api/v4/search?q=";
+
+window.addEventListener("load", () => fetchNews("India"));
+
+function reload(){
+    window.location.reload();
+}
+
+async function fetchNews(query) {
+  const res = await fetch(`${url}${query}&lang=en&country=in&apikey=${API_KEY}`);
+  const data = await res.json();
+  console.log(data);
+  bindData(data.articles);
+}
+
+function bindData(articles) {
+  const cardContainer = document.getElementById("cards-container");
+  const newsCardTemplate = document.getElementById("template-news-card");
+
+  cardContainer.innerHTML = "";
+  articles.forEach((article) => {
+    if (!article.image) {
+      return;
+    }
+    const cardClone = newsCardTemplate.content.cloneNode(true);
+
+    fillDataInCard(cardClone, article);
+    cardContainer.appendChild(cardClone);
+  });
+}
+
+function fillDataInCard(cardClone, article) {
+  const newsImg = cardClone.querySelector("#news-img");
+  const newsTitle = cardClone.querySelector("#news-title");
+  const newsSource = cardClone.querySelector("#news-source");
+  const newsDesc = cardClone.querySelector("#news-desc");
+
+  newsImg.src = article.image;
+  newsTitle.innerHTML = article.title;
+  newsDesc.innerHTML = article.description;
+  const date = new Date(article.publishedAt).toLocaleString("en-US", {
+    timeZone: "Asia/Jakarta",
+  });
+  newsSource.innerHTML = `${article.source.name} · ${date}`;
+  cardClone.firstElementChild.addEventListener("click" ,() => {
+    window.open(article.url, "_blank");
+  })
+}
+
+let curSelectedNav = null;
+function onNavItemClick(id){
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    curSelectedNav?.classList.remove('active');
+    curSelectedNav = navItem;
+    curSelectedNav.classList.add("active");
+}
+
+const searchButton = document.getElementById("search-button");
+const searchText = document.getElementById("search-text");
+
+searchText.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      searchButton.click();
+    }
+  });
+
+searchButton.addEventListener("click" , () => {
+    const query = searchText.value;
+    fetchNews(query);
+    curSelectedNav?.classList.remove('active');
+    curSelectedNav = null;
+})
